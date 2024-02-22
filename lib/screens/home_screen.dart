02ea665f147a8ab2ee0323ba074b202d35f4ba1b
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-02-22 20:28:47 +0600

    home page is done need to work on the product details page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trendy_hive/constants/assets.dart';
 import 'package:trendy_hive/model/local_product.dart';
+import 'package:trendy_hive/screens/product_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  List<String> tabs = ['All', 'Top', 'Category', 'Recommended','Popular','Watch'];
+  final List<String> tabs = [
+    'All',
+    'Top',
+    'Category',
+    'Recommended',
+    'Popular',
+    'Watch'
+  ];
 
   HomeScreen({super.key});
 
@@ -127,7 +135,7 @@ class HomeScreen extends StatelessWidget {
                         child: Container(
                           height: 35.h,
                           margin: EdgeInsets.all(10.sp),
-                          padding: EdgeInsets.only(left: 16.w,right: 16.w),
+                          padding: EdgeInsets.only(left: 16.w, right: 16.w),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10.r),
                             color: Colors.black12.withOpacity(.05),
@@ -158,59 +166,237 @@ class HomeScreen extends StatelessWidget {
                 20.verticalSpace,
                 SizedBox(
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height *.3.h,
+                  height: MediaQuery.of(context).size.height * .25.h,
                   child: ListView.builder(
                     shrinkWrap: true,
                     scrollDirection: Axis.horizontal,
                     itemCount: LocalProduct.generateProductList().length,
-                    itemBuilder: (context,index){
-                      LocalProduct product = LocalProduct.generateProductList()[index];
+                    itemBuilder: (context, index) {
+                      LocalProduct product =
+                          LocalProduct.generateProductList()[index];
+                      return Padding(
+                        padding: EdgeInsets.only(right: 25.w),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              children: [
+                                InkWell(
+                                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const ProductScreen())),
+                                  child: SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        .25.h,
+                                    child: Stack(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(10.r),
+                                          child: Image.asset(product.imageUrl),
+                                        ),
+                                        Positioned(
+                                          right: 10.w,
+                                          top: 10.h,
+                                          child: CircleAvatar(
+                                            backgroundColor: Colors.white,
+                                            radius: 15.r,
+                                            child: Center(
+                                              child: Icon(
+                                                Icons.favorite,
+                                                color: Colors.redAccent,
+                                                size: 20.spMax,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            10.verticalSpace,
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  product.title,
+                                  style: TextStyle(
+                                      fontSize: 15.spMin,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black),
+                                ),
+                                10.verticalSpace,
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * .45.w,
+                                  child: Text(
+                                    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. ",
+                                    style: TextStyle(
+                                        fontSize: 15.spMin,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black),
+                                    maxLines: 6,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                10.verticalSpace,
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Flexible(
+                                      flex: 1,
+                                      child: Icon(
+                                        Icons.star,
+                                        color: Colors.yellow,
+                                        size: 12.spMin,
+                                      ),
+                                    ),
+                                    Flexible(
+                                      flex: 1,
+                                      child: Text(
+                                        '(${product.reviews})',
+                                        style: TextStyle(
+                                            fontSize: 12.spMin,
+                                            fontWeight: FontWeight.w400,
+                                            color: Colors.black),
+                                      ),
+                                    ),
+                                    5.horizontalSpace,
+                                    Flexible(
+                                      flex: 3,
+                                      child: Text(
+                                        product.price,
+                                        style: TextStyle(
+                                            fontSize: 18.spMin,
+                                            fontWeight: FontWeight.w400,
+                                            color: Colors.black),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                20.verticalSpace,
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Newest Products',
+                    style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black),
+                  ),
+                ),
+                20.verticalSpace,
+                GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: LocalProduct.generateProductList().length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: .7,
+                        crossAxisSpacing: 2.w),
+                    itemBuilder: (context, index) {
+                      LocalProduct product =
+                          LocalProduct.generateProductList()[index];
                       return Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           InkWell(
-                            onTap: (){},
-                            child: Padding(
-                              padding:  EdgeInsets.only(right: 16.w),
-                              child: SizedBox(
-                                height: MediaQuery.of(context).size.height *.15.h,
-                                child: Stack(
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(20.r),
-                                      child: Image.asset(product.imageUrl),
+                            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const ProductScreen())),
+                            child: SizedBox(
+                              height: MediaQuery.of(context).size.height * .2.h,
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.r),
+                                    child: Image.asset(product.imageUrl,
+                                        width: MediaQuery.sizeOf(context).width* .45.w,
+                                      fit: BoxFit.fitWidth,
                                     ),
-                                    Positioned(
-                                      right: 5.w,
-                                      top:5.h,
-                                      child: CircleAvatar(
-                                        backgroundColor: Colors.white,
-                                        radius: 10.r,
-                                        child: Center(child: Icon(Icons.favorite,color: Colors.redAccent,size: 15.spMax,)),
+                                  ),
+                                  Positioned(
+                                    right: 5.w,
+                                    top: 5.h,
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.white,
+                                      radius: 10.r,
+                                      child: Center(
+                                        child: Icon(
+                                          Icons.favorite,
+                                          color: Colors.redAccent,
+                                          size: 15.spMax,
+                                        ),
                                       ),
                                     ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
                           10.verticalSpace,
-                          Padding(
-                            padding:  EdgeInsets.only(right: 16.w),
-                            child: Column(
-
-                              children: [
-                                Text(product.title,style: TextStyle(fontSize: 15.spMin,fontWeight: FontWeight.w400,color: Colors.black),),
-                                Text(product.price,style: TextStyle(fontSize: 15.spMin,fontWeight: FontWeight.w400,color: Colors.black),),
-                              ],
-                            ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.title,
+                                style: TextStyle(
+                                    fontSize: 15.spMin,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Flexible(
+                                    flex: 1,
+                                    child: Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                                      size: 12.spMin,
+                                    ),
+                                  ),
+                                  Flexible(
+                                    flex: 1,
+                                    child: Text(
+                                      '(${product.reviews})',
+                                      style: TextStyle(
+                                          fontSize: 12.spMin,
+                                          fontWeight: FontWeight.w400,
+                                          color: Colors.black),
+                                    ),
+                                  ),
+                                  5.horizontalSpace,
+                                  Flexible(
+                                    flex: 3,
+                                    child: Text(
+                                      product.price,
+                                      style: TextStyle(
+                                          fontSize: 18.spMin,
+                                          fontWeight: FontWeight.w400,
+                                          color: Colors.black),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           )
                         ],
                       );
-                    },
-                  ),
-                ),
+                      ;
+                    })
               ],
             ),
           ),
---
+++ lib/screens/product_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        foregroundColor: Colors.black,
+      ),
+    );
+  }
+}
